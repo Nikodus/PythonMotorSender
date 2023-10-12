c8c87fcd5fee061ce0379e74925e0ef32b43d438
Nikodus <niklas.loidls@gmail.com>
2023-10-12 11:36:05 +0200

    Faster Wifi Communication Fix

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# echo-server.py
-
 import socket
 
-HOST = "192.168.243.86"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "192.168.243.86"  # Ip Addresse für das AGV (ESP)
+PORT = 65432  # Port für das AGV (ESP)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -12,4 +10,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
         while True:
-           conn.sendall(bytes(input("Command:"), 'utf-8')+"\r")
+           conn.sendall(bytes(input("Command:")+"\r", 'utf-8'))
